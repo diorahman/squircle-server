adee394d13361dbb3afa24a6a0148dece8ead455
Dhi Aurrahman <diorahman@gmail.com>
2013-03-11 06:46:53 +0700

    change to mongolab

--- config/initializers/02_mongoose.js
+++ config/initializers/02_mongoose.js
@@ -1,7 +1,8 @@
 // Initialize connections
 var mongoose = require('mongoose')
 // mongodb://<dbuser>:<dbpassword>@ds037447.mongolab.com:37447/af_squircle-fog1
-mongoose.connect('mongodb://localhost/squircle')
+// mongoose.connect('mongodb://localhost/squircle')
+mongoose.connect('mongodb://squircle:dio10201@ds037447.mongolab.com:37447/af_squircle-fog1')
 
 var mongooseTypes = require("mongoose-types")
 mongooseTypes.loadTypes(mongoose)
